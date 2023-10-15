6ca8680bae5b28c1b12ba0421c45bb6b2b038090
sharylizareid <59847086+sharylizareid@users.noreply.github.com>
2023-10-15 00:58:05 -0500

    sign up but not really

--- components/SignUp.vue
+++ components/SignUp.vue
@@ -2,18 +2,20 @@
 const user = useSupabaseUser()
 </script>
 <template>
-    <link rel="stylesheet" href= "assets/login.css">
+    <link rel="stylesheet" href= "assets/signup.css">
     <h2 id="siteHead">The SKRRt Scheduler</h2>
-    <h3 id="loginTitle">Log In</h3>
-    <div class="login-background">
+    <h3 id="signupTitle">Sign Up Below!</h3>
+    <div class="signup-background">
         <h3 id="username-label">Username</h3>
         <input type="text" placeholder="Select a Unique Username!" id="username-input">
         
         <h3 id="password-label">Password</h3>
         <input type="text" placeholder="Choose a Strong Password!" id="password-input">
 
-        <input type="submit" value="Let's Go!" id="submit-login">
+        <h3 id="key-label">Authentication Key <i class="fa-solid fa-circle-info" id="key-info" onClick={pressedInfo()}></i></h3>
+        <input type="text"  id="key-input" placeholder="Click the 'i' above to locate your canvas authentication key!">
 
-        <p id="nav-signup">Don't have an account? Register <a href="signup.html">here.</a></p>
+        <input type="submit" value="Lets Go!" id="submit-login">
+        <p id="nav-login">Back to <a href="login.html">login</a></p>
     </div>
 </template>
